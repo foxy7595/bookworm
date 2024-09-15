0a89191821ad19d1b46ffcaef1da70823ff5073f
Foxy <taikhoan759501@gmail.com>
2024-09-15 14:01:16 +0700

    init

---
+++ src/api/term-and-policy/controllers/term-and-policy.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * term-and-policy controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::term-and-policy.term-and-policy');
