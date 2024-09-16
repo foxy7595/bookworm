2e3743e48de167abe71dc86358a2117409eaca43
Foxy <taikhoan759501@gmail.com>
2024-09-16 09:00:39 +0700

    init

---
+++ packages/core/bookworm/src/api/term-and-policy/routes/term-and-policy.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * term-and-policy router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::term-and-policy.term-and-policy');
---
+++ packages/core/bookworm/src/api/term-and-policy/services/term-and-policy.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * term-and-policy service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::term-and-policy.term-and-policy');
